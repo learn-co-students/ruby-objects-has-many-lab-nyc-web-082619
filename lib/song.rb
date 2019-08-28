506d1a2b1b462cc024da720ba5c89782004bfc91
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-28 17:42:56 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+    attr_accessor :title, :artist
+    
+    @@all = []
+
+    def initialize(title, artist="")
+        @title = title
+        @artist = artist
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
